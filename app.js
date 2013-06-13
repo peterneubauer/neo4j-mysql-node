08a3aef9682a1179222a9d4ed454e08b6dc8615a
peterneubauer <peter@neubauer.se>
2013-06-13 15:23:29 +0200

    some first populating the DB

--- app.js
+++ app.js
@@ -1,16 +1,49 @@
-var mysql      = require('mysql');
-var connection = mysql.createConnection({
-    host     : 'localhost',
-    user     : 'me',
-    password : 'secret'
+var mysqlDriver = require('mysql');
+var neo4jDriver = require('neo4j');
+
+var mysql = mysqlDriver.createConnection({
+    host: 'localhost',
+    user: 'me',
+    password: 'secret'
 });
+mysql.connect();
+var neo4j = new neo4jDriver.GraphDatabase('http://localhost:7474');
 
-connection.connect();
 
-connection.query('SELECT 1 + 1 AS solution', function(err, rows, fields) {
+// populate the database with a small PARTS-graph
+
+var sql = [
+    "DROP DATABASE teile",                             ,
+    "CREATE DATABASE teile;",
+    "USE teile;",
+    "CREATE TABLE TEIL (TeilID int, Name VARCHAR(255));"          ,
+    "CREATE TABLE TEIL_VON (TeilID int, UnterTeilID int, Anzahl int);",
+    "INSERT INTO TEIL VALUES(1,'Golf');",
+    "INSERT INTO TEIL VALUES(2,'Karosserie');" ,
+    "INSERT INTO TEIL VALUES(3,'Rad');",
+    "INSERT INTO TEIL_VON VALUES(1,2,1);",
+    "INSERT INTO TEIL_VON VALUES(2,3,4);"
+//    "SELECT * FROM TEIL JOIN TEIL_VON ON TEIL_VON.TeilID=TEIL.TeilID AND TEIL_VON.UnterTeilID=TEIL.TeilID,;"
+];
+
+
+sql.forEach(function (statement) {
+        console.log(statement);
+        mysql.query(statement, function (err, rows, fields) {
+            if (err) throw err;
+
+            console.log(rows);
+        });
+    }
+)
+neo4j.query("START n=node(*) return n;", {}, function (err, results) {
     if (err) throw err;
-
-    console.log('The solution is: ', rows[0].solution);
+    var rel = results[0] && results[0]['rel'];
+    console.log( results);
 });
 
-connection.end();
+
+
+mysql.end();
+
+
